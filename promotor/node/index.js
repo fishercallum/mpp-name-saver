21f886a0c9b1d6aaf11fce90ca60b1534d5bfb0e
Callum Fisher <cf.fisher.bham@gmail.com>
2025-01-07 17:47:19 +0000

    Correct file name in comments.

--- promotor/node/index.js
+++ promotor/node/index.js
@@ -1,7 +1,7 @@
 /*
 	"Name Saver for Multiplayer Piano - Promotor - Node.js"
-	ad-node.js - Main app.
-	2025.01.04 - 2025.01.07 (v1.0.4)
+	index.js - Main app.
+	2025.01.04 - 2025.01.07 (v1.0.5)
 
 	Created to promote the Name Saver userscript - bit.ly/SaveOurNames
 
