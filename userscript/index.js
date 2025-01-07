09bca215c76557cb34a51c324362827d17d22b16
Callum Fisher <cf.fisher.bham@gmail.com>
2025-01-07 17:59:57 +0000

    Change startNameSaver to startMPPNameSaver

--- userscript/index.js
+++ userscript/index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Name Saver for Multiplayer Piano
 // @namespace    https://github.com/slowstone72/MPP-name-saver
-// @version      1.0.3
+// @version      1.1.0
 // @license      Unlicense
 // @description  Auto-Save & Set your nickname on Multiplayer Piano. No more 'Anonymous.'
 // @author       Callum Fisher <cf.fisher.bham@gmail.com>
@@ -48,13 +48,13 @@
 	For more information, please refer to <https://unlicense.org/>
 */
 
-if (typeof startNameSaver === 'function') {
-	console.log('[Name Saver for Multiplayer Piano v1.0.3] Script is already running! No need to restart.');
+if (typeof startMPPNameSaver === 'function') {
+	console.log('[Name Saver for Multiplayer Piano v1.1.0] Script is already running! No need to restart.');
 	return;
 }
 
-const startNameSaver = () => {
-	console.log('[Name Saver for Multiplayer Piano v1.0.3] Running.');
+const startMPPNameSaver = () => {
+	console.log('[Name Saver for Multiplayer Piano v1.1.0] Running.');
 	const setName = () => {
 		MPP.client.sendArray([{
 			'm': 'userset',
@@ -85,7 +85,7 @@ const startNameSaver = () => {
 // Start:
 
 if (!window.addEventListener) {
-	window.attachEvent('onload', startNameSaver);
+	window.attachEvent('onload', startMPPNameSaver);
 } else {
-	window.addEventListener('load', startNameSaver);
+	window.addEventListener('load', startMPPNameSaver);
 }
