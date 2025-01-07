157030e72c8b704044e5d9c2cfe74645a14bf45c
Callum Fisher <cf.fisher.bham@gmail.com>
2025-01-07 13:52:00 +0000

    Switch to semantic versioning.

--- userscript/index.js
+++ userscript/index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Name Saver for Multiplayer Piano
 // @namespace    https://github.com/slowstone72/MPP-name-saver
-// @version      2025.01.05
+// @version      1.0.0
 // @license      Unlicense
 // @description  Auto-Save & Set your nickname on Multiplayer Piano. No more 'Anonymous.'
 // @author       Callum Fisher <cf.fisher.bham@gmail.com>
