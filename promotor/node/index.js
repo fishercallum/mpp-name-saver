5850b5deb1d21b37e9ae3eecd5df9e635ae39fab
Callum Fisher <cf.fisher.bham@gmail.com>
2025-01-07 17:40:52 +0000

    Move WS server definition to wsServer string.

--- promotor/node/index.js
+++ promotor/node/index.js
@@ -1,7 +1,7 @@
 /*
 	"Name Saver for Multiplayer Piano - Promotor - Node.js"
 	ad-node.js - Main app.
-	2025.01.04 - 2025.01.06 (v1.0.2)
+	2025.01.04 - 2025.01.07 (v1.0.3)
 
 	Created to promote the Name Saver userscript - bit.ly/SaveOurNames
 
@@ -41,6 +41,8 @@ const Client = require('./Client.js');
 
 // Define settings:
 
+const wsServer = 'wss://game.multiplayerpiano.com:443';
+
 const ad = 'Hi, %name%. M.P.P. doesn\'t save your nickname by default, so I made a simple userscript to try to fix that. If you\'re interested, try it out: bit.ly/SaveOurNames (https://greasyfork.org/scripts/522853)';
 const desiredChannel = 'test/Save Your Nickname';
 const desiredName = '[bit.ly/SaveOurNames]';
@@ -53,7 +55,7 @@ let lastMessage = {
 
 // Create new Multiplayer Piano client instance:
 
-const client = new Client('wss://game.multiplayerpiano.com:443');
+const client = new Client(wsServer);
 
 // Define functions:
 
